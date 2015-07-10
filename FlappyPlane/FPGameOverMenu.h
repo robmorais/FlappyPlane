c8d63d2eba23a121d358c222a44a1436a6c689ac
Roberto Silva <roberto@felloway.com>
2015-07-10 11:29:08 +0100

    Create a class for the game over menu and added some assets to be used on it.

---
+++ FlappyPlane/FPGameOverMenu.h
@@ -0,0 +1,13 @@
+//
+//  TPGameOverMenu.h
+//  FlappyPlane
+//
+//  Created by Roberto Silva on 10/07/15.
+//  Copyright (c) 2015 HE:mobile. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface FPGameOverMenu : SKNode
+
+@end
---
+++ FlappyPlane/FPGameOverMenu.m
@@ -0,0 +1,13 @@
+//
+//  TPGameOverMenu.m
+//  FlappyPlane
+//
+//  Created by Roberto Silva on 10/07/15.
+//  Copyright (c) 2015 HE:mobile. All rights reserved.
+//
+
+#import "FPGameOverMenu.h"
+
+@implementation FPGameOverMenu
+
+@end
