3adddcda326c8ce97231e02d1b14b0efb8ea16fc
Roberto Silva <roberto@felloway.com>
2015-07-10 18:01:38 +0100

    Setup titles for score, best score and medal on the game over menu class.

--- FlappyPlane/FPGameOverMenu.m
+++ FlappyPlane/FPGameOverMenu.m
@@ -16,8 +16,14 @@
     if (self = [super init]) {
         _size = size;
         
+        // Get texture atlas
         SKTextureAtlas *atlas = [SKTextureAtlas atlasNamed:kFPGraphicsAtlas];
         
+        // Create an overravel node to put all other nodes
+        SKNode *panelGroup = [SKNode node];
+        [self addChild:panelGroup];
+        
+        // Setup panel background
         SKSpriteNode *panelBG = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"UIbg"]];
         panelBG.position = CGPointMake(size.width * 0.5, size.height - 150);
         panelBG.centerRect = CGRectMake(10/panelBG.size.width, 10/panelBG.size.height,
@@ -25,9 +31,27 @@
                                         (panelBG.size.height-20)/panelBG.size.height);
         
         panelBG.xScale = 175./panelBG.size.width;
-        panelBG.yScale = 150./panelBG.size.height;
+        panelBG.yScale = 115./panelBG.size.height;
         
-        [self addChild:panelBG];
+        [panelGroup addChild:panelBG];
+        
+        // Setup score title
+        SKSpriteNode *scoreTitle = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"textScore"]];
+        scoreTitle.anchorPoint = CGPointMake(1.0, 1.0);
+        scoreTitle.position = CGPointMake(CGRectGetMaxX(panelBG.frame) - 20, CGRectGetMaxY(panelBG.frame) - 10);
+        [panelGroup addChild:scoreTitle];
+        
+        // Setup best score title
+        SKSpriteNode *bestScoreTitle = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"textBest"]];
+        bestScoreTitle.anchorPoint = CGPointMake(1.0, 1.0);
+        bestScoreTitle.position = CGPointMake(CGRectGetMaxX(panelBG.frame) - 20, CGRectGetMaxY(panelBG.frame) - 60);
+        [panelGroup addChild:bestScoreTitle];
+        
+        // Setup medal title
+        SKSpriteNode *medalTitle = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"textMedal"]];
+        medalTitle.anchorPoint = CGPointMake(0.0, 1.0);
+        medalTitle.position = CGPointMake(CGRectGetMinX(panelBG.frame) + 20, CGRectGetMaxY(panelBG.frame) - 10);
+        [panelGroup addChild:medalTitle];
         
     }
     
