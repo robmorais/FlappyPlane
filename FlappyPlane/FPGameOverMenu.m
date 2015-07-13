f2a39ca5d88f111a7f8d5f5cca7f42b5c3ca4528
Roberto Silva <roberto@felloway.com>
2015-07-13 11:58:33 +0100

    Setup score and best score text on the game over menu

--- FlappyPlane/FPGameOverMenu.m
+++ FlappyPlane/FPGameOverMenu.m
@@ -8,9 +8,12 @@
 
 #import "FPGameOverMenu.h"
 #import "FPConstants.h"
+#import "FPBitmapFontLabel.h"
 
 @interface FPGameOverMenu()
 @property (nonatomic) SKSpriteNode *medalDisplay;
+@property (nonatomic) FPBitmapFontLabel *scoreText;
+@property (nonatomic) FPBitmapFontLabel *bestScoreText;
 @end
 
 @implementation FPGameOverMenu
@@ -45,11 +48,25 @@
         scoreTitle.position = CGPointMake(CGRectGetMaxX(panelBG.frame) - 20, CGRectGetMaxY(panelBG.frame) - 10);
         [panelGroup addChild:scoreTitle];
         
+        // Setup score text label
+        _scoreText = [[FPBitmapFontLabel alloc] initWithText:@"0" andFontName:@"number"];
+        _scoreText.alignment = BitmapFontAlignmentRight;
+        _scoreText.position = CGPointMake(CGRectGetMaxX(scoreTitle.frame),CGRectGetMinY(scoreTitle.frame) - 15);
+        [_scoreText setScale:0.5];
+        [panelGroup addChild:_scoreText];
+        
         // Setup best score title
         SKSpriteNode *bestScoreTitle = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"textBest"]];
         bestScoreTitle.anchorPoint = CGPointMake(1.0, 1.0);
         bestScoreTitle.position = CGPointMake(CGRectGetMaxX(panelBG.frame) - 20, CGRectGetMaxY(panelBG.frame) - 60);
         [panelGroup addChild:bestScoreTitle];
+        
+        // Setup best score text label
+        _bestScoreText = [[FPBitmapFontLabel alloc] initWithText:@"0" andFontName:@"number"];
+        _bestScoreText.alignment = BitmapFontAlignmentRight;
+        _bestScoreText.position = CGPointMake(CGRectGetMaxX(bestScoreTitle.frame),CGRectGetMinY(bestScoreTitle.frame) - 15);
+        [_bestScoreText setScale:0.5];
+        [panelGroup addChild:_bestScoreText];
         
         // Setup medal title
         SKSpriteNode *medalTitle = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"textMedal"]];
@@ -65,10 +82,24 @@
         
         // Set initial values
         self.medal = MedalNone;
+        self.score = 78;
+        self.bestScore = 340;
         
     }
     
     return self;
+}
+
+- (void)setScore:(NSInteger)score
+{
+    _score = score;
+    self.scoreText.text = [NSString stringWithFormat:@"%ld",(long)score];
+}
+
+- (void)setBestScore:(NSInteger)bestScore
+{
+    _bestScore = bestScore;
+    self.bestScoreText.text = [NSString stringWithFormat:@"%ld",(long)bestScore];
 }
 
 - (void)setMedal:(MedalType)medal
